226b2fa19b39d29cdc7907212be1f491d02c1bd2
keepmesmerizing <keepmesmerizing@gmail.com>
2025-07-24 10:36:32 -0700

    Update v1.lua

--- src/v1.lua
+++ src/v1.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Patheticcs/Soluna-API/refs/heads/main/src/api/v1.lua",true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/keepmesmerizing/Soluna-API/refs/heads/main/src/api/v1.lua",true))()
