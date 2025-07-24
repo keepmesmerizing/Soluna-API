ad2d09ce4236920ea51624c03553e0f8327c839b
keepmesmerizing <keepmesmerizing@gmail.com>
2025-07-24 10:36:12 -0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Patheticcs/Soluna-API/refs/heads/main/src/main.lua",true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/keepmesmerizing/Soluna-API/refs/heads/main/src/main.lua",true))()
